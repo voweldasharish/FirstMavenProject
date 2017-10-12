b8526cf8312268e5564bb11d96bef6bb0c36749c
Harish Kumar <v.a.harish.kumar@accenture.com>
2017-10-12 12:54:35 +0530

    Maven project commit to GITHUB

---
+++ src/main/java/Testing_OCT/TestingCignaDev/App.java
@@ -0,0 +1,13 @@
+package Testing_OCT.TestingCignaDev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
